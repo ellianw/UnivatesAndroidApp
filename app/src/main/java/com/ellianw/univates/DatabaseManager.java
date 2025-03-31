86db6153a242c28658da0d1cc857854453857aa4
ellianw <elliangwolfart@gmail.com>
2025-03-30 22:48:35 -0300

    SQL data manipulation

---
+++ app/src/main/java/com/ellianw/univates/DatabaseManager.java
@@ -0,0 +1,22 @@
+package com.ellianw.univates;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseManager extends SQLiteOpenHelper {
+    public DatabaseManager(Context context, String databaseName, int version){
+        super(context, databaseName, null, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE item (id INTEGER NOT NULL PRIMARY KEY, descricao TEXT, quantidade INTEGER);");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE item");
+        onCreate(db);
+    }
+}
--- app/src/main/java/com/ellianw/univates/MainActivity.java
+++ app/src/main/java/com/ellianw/univates/MainActivity.java
@@ -1,13 +1,149 @@
 package com.ellianw.univates;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    private EditText etItem = null;
+    private EditText etQuantidade = null;
+    private Button btDelete = null;
+    private Button btEdit = null;
+    private DatabaseManager dataBaseManager = null;
+    private SQLiteDatabase bancoDeDados = null;
+    private ListView itemsList = null;
+    private int lastSelectedPosition = 0;
+    private String editedItemName = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        dataBaseManager = new DatabaseManager(this, "aplicacaodb", 1);
+        bancoDeDados = dataBaseManager.getWritableDatabase();
+
+        etItem = findViewById(R.id.name);
+        etQuantidade = findViewById(R.id.quantity);
+        itemsList = findViewById(R.id.listview);
+        btDelete = findViewById(R.id.bt_delete);
+        btEdit = findViewById(R.id.bt_edit);
+
+        Button btBotaoEnviar = findViewById(R.id.bt_send);
+        btBotaoEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ContentValues valores = new ContentValues();
+                valores.put("descricao", etItem.getText().toString());
+                valores.put("quantidade", etQuantidade.getText().toString());
+
+                if (editedItemName != null) {
+                    deleteFromDB(editedItemName);
+                }
+                bancoDeDados = dataBaseManager.getWritableDatabase();
+                long resultado = bancoDeDados.insert("item", null, valores);
+                bancoDeDados.close();
+
+                if (resultado == -1)
+                    Toast.makeText(MainActivity.this, "Não foi possível inserir este item", Toast.LENGTH_SHORT).show();
+                else {
+                    Toast.makeText(MainActivity.this, "Item inserido: " + etItem.getText() + " n: " + etQuantidade.getText(), Toast.LENGTH_SHORT).show();
+                }
+                clearForm(findViewById(R.id.root));
+                fillList();
+                editedItemName = null;
+            }
+        });
+        fillList();
+        itemsList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                btDelete.setEnabled(true);
+                btEdit.setEnabled(true);
+                lastSelectedPosition = position;
+            }
+        });
+        btDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Object deletedItem = itemsList.getItemAtPosition(lastSelectedPosition);
+                String objString = deletedItem.toString().split("\\|")[0].trim();
+                deleteFromDB(objString);
+                fillList();
+            }
+        });
+        btEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Object editItem = itemsList.getItemAtPosition(lastSelectedPosition);
+                String objString = editItem.toString().split("\\|")[0].trim();
+                setEditItem(objString);
+                fillList();
+            }
+        });
+    }
+
+    private void fillList(){
+        itemsList.setAdapter(null);
+        btDelete.setEnabled(false);
+        btEdit.setEnabled(false);
+        bancoDeDados = dataBaseManager.getReadableDatabase();
+        Cursor iCursor = bancoDeDados.rawQuery("SELECT * FROM item", null);
+        iCursor.moveToFirst();
+        ArrayList<String> items = new ArrayList<>();
+        for (int i = 1; i <= iCursor.getCount(); i++) {
+            String name = iCursor.getString(iCursor.getColumnIndexOrThrow("descricao"));
+            String qtd = iCursor.getString(iCursor.getColumnIndexOrThrow("quantidade"));
+            items.add(name+" | "+qtd);
+            iCursor.moveToNext();
+        }
+        itemsList.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_activated_1, items);
+        itemsList.setAdapter(adapter);
+        bancoDeDados.close();
+    }
+
+    private void setEditItem(String itemName) {
+        bancoDeDados = dataBaseManager.getReadableDatabase();
+        Cursor iCursor = bancoDeDados.rawQuery("SELECT * FROM item WHERE descricao = '"+itemName+"'", null);
+        editedItemName = itemName;
+        iCursor.moveToFirst();
+        String name = iCursor.getString(iCursor.getColumnIndexOrThrow("descricao"));
+        String qtd = iCursor.getString(iCursor.getColumnIndexOrThrow("quantidade"));
+        etItem.setText(name);
+        etQuantidade.setText(qtd);
+        fillList();
+        bancoDeDados.close();
+    }
+
+    private void deleteFromDB(String itemName){
+        bancoDeDados = dataBaseManager.getWritableDatabase();
+        bancoDeDados.delete("item","descricao = ?",new String[]{itemName});
+        bancoDeDados.close();
+    }
+
+    private void clearForm(ViewGroup group) {
+        for (int i = 0, count = group.getChildCount(); i < count; ++i) { // Loop pelo ViewGroup
+            View view = group.getChildAt(i);
+            if (view instanceof EditText) { //Se a view for um EditText
+                ((EditText) view).setText("");
+                continue;
+            }
+            if (view instanceof ViewGroup && (((ViewGroup) view).getChildCount() > 0)) //Se for outro ViewGroup
+                clearForm((ViewGroup) view); //Faz uma chamada recursiva
+        }
     }
 }
